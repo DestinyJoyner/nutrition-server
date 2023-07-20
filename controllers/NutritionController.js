1cd4b94bfd912b6030cb4f06872f04bf5a7cab31
PESolut <jahaadpetty@pursuit.org>
2023-07-20 18:54:32 -0400

    page limit added to each page if use search query page

--- controllers/NutritionController.js
+++ controllers/NutritionController.js
@@ -6,10 +6,34 @@ const {
     getOne
 } = require("../query.js")
 
+const isPageQueryADigit = (pageQuery) => {
+    const pageAsInteger = parseInt(pageQuery)
+    const digitRegex = /^\d$/;
+    return digitRegex.test(pageAsInteger);
+  };
+
 router.get("/", async (req, res) => {
     const all = await getAll()
 
-    res.status(200).json(all)
+    //check if the page parameter exists
+    if(req.query.page) {
+        // check that the page parameter is a valid digit
+        if(!isPageQueryADigit(req.query.page)){
+            throw { message: 'page must be a valid digit!', status: 'error'}
+        }
+
+        // calculate the start and end index of the itmes for the requested page
+        const itemsPerPage = 50
+        const page = parseInt(req.query.page)
+        const startIndex = (page - 1) * itemsPerPage
+        const endIndex = startIndex + itemsPerPage;
+
+        const storeItemsRange = all.slice(startIndex, endIndex);
+
+        res.status(200).json(storeItemsRange)
+    } else {
+        res.status(200).json(all)
+    }
 })
 
 router.get("/:id", async (req,res) => {
